03d7ee55d5e95e29b7fc9c41d1bf4975298ac822
jibanprakashbiswal <jibanprakashbiswal2000@gmail.com>
2026-06-13 00:52:17 +0530

    first commit

--- EmployeeProject/src/EmpData.java
+++ EmployeeProject/src/EmpData.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
 
-public class EmpData {
+public class EmpData implements Comparable<EmpData>{
+
+    int EmpId;
+    String EmpName;
+    double EmpSalary;
+
+    public EmpData(int EmpId, String EmpName, double Empsalary) {
+        this.EmpId = EmpId;
+        this.EmpName = EmpName;
+        this.EmpSalary = Empsalary;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Id=" + EmpId +
+                ", Name=" + EmpName + " " +
+                ", Salary=" + EmpSalary
+                ;
+    }
+
+    @Override
+    public int compareTo(EmpData o) {
+        return EmpId-o.EmpId;
+    }
 }
--- EmployeeProject/src/Employee.java
+++ EmployeeProject/src/Employee.java
@@ -1,11 +1,25 @@
-
-public class Employee {
-
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 
+public class Employee extends Thread {
 
+    @Override
+    public void run() {
+    ArrayList emp = new ArrayList();
+      emp.add(new EmpData(103,"Sanajay",10000.50));
+      emp.add(new EmpData(105,"Ajaya",20000.30));
+      emp.add(new EmpData(101,"Ramesh",16000.80));
+      emp.add(new EmpData(102,"Kartik",35000.70));
+
+      //  System.out.println(emp);
+    Collections.sort(emp);
+        System.out.println(emp);
+
+    }
     public static void main(String[] args) {
 
+        Thread t1=new Thread(new Employee());
+        t1.start();
     }
 }
